e60029634fd15ca33b729720b858031d82a99333
Christopher Valarida <chris.valarida@adhocteam.us>
2021-07-19 13:59:59 -0700

    Try to test updating the state

--- test/form-builder/TextField.test.tsx
+++ test/form-builder/TextField.test.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import TextField from '../../src/form-builder/TextField';
+import { fireEvent, waitFor } from '@testing-library/react';
 
 import { buildRenderForm } from '../utils';
 
@@ -16,7 +17,38 @@ describe('Form Builder - TextField', () => {
     expect(input?.getAttribute('label')).toEqual('The Thing');
   });
 
-  test('updates the formik state', () => {});
-
   test('renders the validation error message', () => {});
+
+  test('renders initial value', () => {});
+
+  /**
+   * I'm not sure what to do here. When the event fires
+   * - It's caught by the React wrapper
+   *   - I verified this by changing the event handler to log something
+   * - It runs the validation
+   *   - I added a console.log() to the validation function in TextField that
+   *     only fires after the event is dispatched
+   *
+   * Additionally, it works as expected in my sandbox project. I just don't know
+   * how to test that it updates the state. I expect it's something to do with
+   * the helper.
+   */
+  test.skip('updates the formik state', async () => {
+    const { container, getFormProps, rerender } = renderForm(
+      <TextField name="thing" label="The Thing" />
+    );
+    const input = container.querySelector('va-text-input');
+    if (!input) throw new Error('No va-text-input found');
+
+    // Simulate the input changing without hooking up the web component
+    const changeEvent = new CustomEvent('vaChange', {
+      detail: { value: 'asdf' },
+    });
+
+    fireEvent(input, changeEvent);
+    await waitFor(() => {
+      rerender();
+      expect(getFormProps().values).toEqual({ thing: 'asdf' });
+    });
+  });
 });
