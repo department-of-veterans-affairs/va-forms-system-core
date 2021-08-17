e8fcd85a11735a84f4469ad7e180584f2856a499
Brooks Johnson <brooks.johnson@pm.me>
2021-08-16 17:17:57 -0700

    Replace the wrapper with the React binding

--- src/form-builder/SelectField.tsx
+++ src/form-builder/SelectField.tsx
@@ -5,41 +5,15 @@ import { useField, FieldHookConfig } from 'formik';
 import { FieldProps } from './types';
 import { getMessage } from '../utils/i18n';
 
+import { VaSelect } from 'web-components/react-bindings';
+
 type SelectProps = FieldProps<string> & {
   onVaSelect: (e: CustomEvent) => void;
   children: any;
 };
 
-// TODO: Figure out how to actually import the type defintions for these web components
-// The @ts-ignore comments are because the web component types aren't available.
-const Wrapper = (props: SelectProps) => {
-  const [field, meta] = useField(props as FieldHookConfig<string>);
-
-  // TODO: Use the web component type
-  const ref = useRef<HTMLElement>(null);
-  useEffect(() => {
-    // @ts-ignore
-    ref.current.addEventListener('vaSelect', field.onChange);
-    // @ts-ignore
-    ref.current.addEventListener('vaSelect', props.onVaSelect);
-  }, [field.onChange, props.onVaSelect]);
-
-  // TODO: Try using the <ErrorMessage> component
-  return (
-    // @ts-ignore
-    <va-select
-      ref={ref}
-      {...props}
-      {...field}
-      error={(meta.touched && meta.error) || undefined}
-    >
-      {props.children}
-      {/** @ts-ignore */}
-    </va-select>
-  );
-};
-
 const SelectField = (props: SelectProps): JSX.Element => {
+  const [field, meta] = useField(props as FieldHookConfig<string>);
   const id = props.id || props.name;
 
   const validate = (value: string) => {
@@ -53,7 +27,19 @@ const SelectField = (props: SelectProps): JSX.Element => {
 
     return props.validate ? props.validate(value) : undefined;
   };
-  return <Wrapper id={id} {...props} validate={validate} />;
+
+  const { required, ...nonRequired } = props;
+  return (
+    <VaSelect
+      id={id}
+      label={props.label}
+      required={!!props.required}
+      {...field}
+      onVaSelect={field.onChange}
+    >
+      {props.children}
+    </VaSelect>
+  );
 };
 
 export default SelectField;
