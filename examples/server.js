021f42578c853db90b39c1e8f2e277d594fbdebc
Christopher Valarida <chris.valarida@adhocteam.us>
2021-08-18 16:03:14 -0700

    Add an express server with hot recompile
    Not technically hot module reloading since it doesn't change in the
    browser, but it's the next best thing. Just refresh the page. :shrug:

---
+++ examples/server.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+const express = require('express');
+const path = require('path');
+const webpack = require('webpack');
+const config = require('./webpack.config');
+const devMiddleware = require('webpack-dev-middleware');
+const hotReload = require('webpack-hot-middleware');
+
+const app = express();
+const port = 3000;
+const compiler = webpack(config);
+
+app.use(devMiddleware(compiler));
+app.use(hotReload(compiler));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
