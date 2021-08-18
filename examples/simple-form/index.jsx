68b1eaf8471e517b4206e30d76ba4568aa496e45
Christopher Valarida <chris.valarida@adhocteam.us>
2021-08-18 16:42:52 -0700

    Add a simple form

--- examples/simple-form/index.jsx
+++ examples/simple-form/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>This is a simple form</h1>,
-  document.getElementById('root')
+import { Formik, Form } from 'formik';
+import {
+  TextField,
+  DebuggerView,
+} from '@department-of-veterans-affairs/formulate';
+
+import { defineCustomElements } from 'web-components/loader';
+
+void defineCustomElements();
+
+const App = () => (
+  <div
+    style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}
+  >
+    <h1>Example form</h1>
+    <Formik initialValues={{ foo: '' }}>
+      <Form>
+        <TextField name="foo" label="Example" required />
+        <DebuggerView />
+      </Form>
+    </Formik>
+  </div>
 );
+
+ReactDOM.render(<App />, document.getElementById('root'));
