cc72baa0f1d4a6722773f7112601bf993b44bf29
Christopher Valarida <chris.valarida@adhocteam.us>
2021-09-01 17:09:24 -0700

    Add types and basic value conversion

--- src/form-builder/DateField.tsx
+++ src/form-builder/DateField.tsx
@@ -6,11 +6,53 @@ import { validator } from '../utils/validation';
 
 import Date from '@department-of-veterans-affairs/component-library/Date';
 
-const DateField = (props: FieldProps<string>): JSX.Element => {
+export type ComponentLibraryDatePart = {
+  value: string;
+  dirty: boolean;
+};
+
+/**
+ * What the Date in the component library expects.
+ */
+export type ComponentLibraryDateValue = {
+  date: {
+    day: ComponentLibraryDatePart;
+    month: ComponentLibraryDatePart;
+    year: ComponentLibraryDatePart;
+  };
+};
+
+export type DateProps = FieldProps<string> & {
+  /**
+   * Format: MM-DD-YYYY
+   */
+  date: string;
+};
+
+/**
+ * Convert the date string passed to DateField to the object expected by the
+ * component library Date component.
+ */
+const dateStringToValue = (dateString = '') => {
+  // TODO: Make this safer
+  const [month = '', day = '', year = ''] = dateString.split('-');
+  return {
+    day: { value: day, dirty: false },
+    month: { value: month, dirty: false },
+    year: { value: year, dirty: false },
+  };
+};
+
+const DateField = (props: DateProps): JSX.Element => {
   const withValidation = { ...props, validate: validator(props) };
   const [field, meta] = useField(withValidation as FieldHookConfig<string>);
   const id = props.id || props.name;
-  return <Date id={id} {...props} onValueChange={field.onChange} />;
+
+  const value = dateStringToValue(props.date);
+
+  return (
+    <Date id={id} {...props} onValueChange={field.onChange} date={value} />
+  );
 };
 
 export default DateField;
