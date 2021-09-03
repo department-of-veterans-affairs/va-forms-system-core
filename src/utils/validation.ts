b6026bf3ff79bc413507bfee3362dc98d5b269b6
Christopher Valarida <chris.valarida@adhocteam.us>
2021-09-02 17:38:31 -0700

    Add validation chainer

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,25 +1,44 @@
 import { FieldProps } from '../form-builder/types';
 import { getMessage } from './i18n';
 
-export type ValidationFunctionResult<T> = (
-  value: T
-) => void | undefined | string | Promise<any>;
+export type ValidationFunctionResult<T> =
+  | void
+  | undefined
+  | string
+  | Promise<any>;
 export type ValidationFunction<T> = (
+  value: T,
   props: FieldProps<T>
 ) => ValidationFunctionResult<T>;
 
-export const addRequired = <T>(
-  props: FieldProps<T>
-): ValidationFunctionResult<T> => {
+export const chainValidations = <T>(
+  props: FieldProps<T>,
+  validations: ValidationFunction<T>[]
+): ((value: T) => ValidationFunctionResult<T>) => {
   return (value: T) => {
-    if (props.required && !value) {
-      const errorMessage =
-        typeof props.required === 'string'
-          ? props.required
-          : getMessage('required.default');
-      return errorMessage;
-    }
+    // Return the error message from the first validation function that fails.
+    const errorMessage = validations
+      .map((v) => v(value, props))
+      .filter((m) => m)[0];
+    if (errorMessage) return errorMessage;
 
+    // None of the built-in validation functions failed; run the validate
+    // function passed to the component.
     return props.validate ? props.validate(value) : undefined;
   };
+};
+
+export const addRequired = <T>(
+  value: T,
+  props: FieldProps<T>
+): ValidationFunctionResult<T> => {
+  if (props.required && !value) {
+    const errorMessage =
+      typeof props.required === 'string'
+        ? props.required
+        : getMessage('required.default');
+    return errorMessage;
+  }
+
+  return props.validate ? props.validate(value) : undefined;
 };
