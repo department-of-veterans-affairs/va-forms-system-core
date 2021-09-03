cb3559deece6fb295ae0c87f84dc76c56ed716ed
Christopher Valarida <chris.valarida@adhocteam.us>
2021-09-02 17:43:15 -0700

    Chain validations in SelectField

--- src/form-builder/SelectField.tsx
+++ src/form-builder/SelectField.tsx
@@ -1,8 +1,8 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import { useField, FieldHookConfig } from 'formik';
 
 import { FieldProps } from './types';
-import { addRequired } from '../utils/validation';
+import { chainValidations, required } from '../utils/validation';
 
 import { VaSelect } from 'web-components/react-bindings';
 
@@ -12,7 +12,10 @@ type SelectProps = FieldProps<string> & {
 };
 
 const SelectField = (props: SelectProps): JSX.Element => {
-  const withValidation = { ...props, validate: addRequired(props) };
+  const withValidation = {
+    ...props,
+    validate: chainValidations(props, [required]),
+  };
   const [field, meta] = useField(withValidation as FieldHookConfig<string>);
   const id = props.id || props.name;
 
