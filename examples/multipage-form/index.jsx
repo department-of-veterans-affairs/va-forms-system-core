46ab9b72408c417b3b2507d995903ecb9b662f13
Brooks Johnson <brooks.johnson@pm.me>
2021-09-11 21:30:07 -0700

    Inline the example

--- examples/multipage-form/index.jsx
+++ examples/multipage-form/index.jsx
@@ -16,27 +16,19 @@ import { defineCustomElements } from 'web-components/loader';
 
 void defineCustomElements();
 
-const page1 = (
-  <Page title="Example form" path="/one" next="/two">
-    <TextField name="foo" label="Example" required />
-    <DebuggerView />
-  </Page>
-);
-
-const page2 = (
-  <Page title="Second page" path="/two" previous="/one">
-    <TextField name="foo" label="Example" required />
-    <CheckboxField name="bar" label="Do you have pets?" required />
-    <DebuggerView />
-  </Page>
-);
-
 const App = () => (
   <Router basename="multipage-form">
-    {page1}
-    {page2}
+    <Page title="Example form" path="/one" next="/two">
+      <TextField name="foo" label="Example" required />
+      <DebuggerView />
+    </Page>
+
+    <Page title="Second page" path="/two" previous="/one">
+      <CheckboxField name="bar" label="Do you have pets?" required />
+      <DebuggerView />
+    </Page>
+
     <Route path="/">
-      <div>Whatever:</div>
       <Link to="one">Page one</Link>
     </Route>
   </Router>
