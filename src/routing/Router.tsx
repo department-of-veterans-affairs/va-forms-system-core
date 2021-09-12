5de178ef0aee820c8d96dcf0d4dc8889cceb0ca6
Brooks Johnson <brooks.johnson@pm.me>
2021-09-11 21:33:11 -0700

    Move RouterProps to types file

--- src/routing/Router.tsx
+++ src/routing/Router.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import { PageType } from './types';
+import { RouterProps } from './types';
 import { BrowserRouter, Switch } from 'react-router-dom';
-
-interface RouterProps {
-  children: Array<PageType>;
-  basename: string;
-  history: any; // TODO: It's the browser history...
-}
 
 export default function Router(props: RouterProps): JSX.Element {
   return (
--- src/routing/types.ts
+++ src/routing/types.ts
@@ -1,3 +1,8 @@
+export interface RouterProps {
+  children: Array<PageType>;
+  basename: string;
+}
+
 export interface PageType extends React.Component {
   props: {
     path: string; // Used by the Router directly
