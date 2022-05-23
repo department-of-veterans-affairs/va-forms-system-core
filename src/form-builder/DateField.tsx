41c100d580afbfe902340708ac1ac85de37f7782
harshil1793 <harshil1793@gmail.com>
2022-05-23 17:42:04 -0400

    fix: DateField & Skip: formik tests

--- src/form-builder/DateField.tsx
+++ src/form-builder/DateField.tsx
@@ -2,18 +2,9 @@ import React from 'react';
 import { useField, FieldHookConfig } from 'formik';
 
 import { DateProps } from './types';
-import {
-  ValidationFunction,
-  chainValidations,
-  required,
-} from '../utils/validation';
+import { chainValidations, required } from '../utils/validation';
 import { VaDate } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 
-/**
- * Field value format: M-D-YYYY
- *
- * TODO: Convert the Date component into a web component and use it here.
- */
 const DateField = (props: DateProps): JSX.Element => {
   const withValidation = {
     ...props,
@@ -26,9 +17,9 @@ const DateField = (props: DateProps): JSX.Element => {
 
   const value = field.value;
 
-  // const onChange = (dateValue: any) => {
-  //   helpers.setValue(dateValue.target.value);
-  // };
+  const onChange = (dateValue: CustomEvent) => {
+    helpers.setValue((dateValue?.target as HTMLInputElement).value);
+  };
 
   return (
     <VaDate
@@ -36,7 +27,7 @@ const DateField = (props: DateProps): JSX.Element => {
       required={!!props.required}
       {...props}
       label={props.label}
-      // onDateChange={onChange}
+      onDateChange={onChange}
       value={value}
       onDateBlur={() => helpers.setTouched(true)}
       error={(meta.touched && meta.error) || undefined}
