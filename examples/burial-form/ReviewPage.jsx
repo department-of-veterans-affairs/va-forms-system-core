e2018044ddc6104a2c3dd030833192fad110f331
Lauren Maxwell <lauren.maxwell@adhocteam.us>
2022-06-02 13:31:37 -0700

    Adds updates to page.

--- examples/burial-form/ReviewPage.jsx
+++ examples/burial-form/ReviewPage.jsx
@@ -6,37 +6,15 @@ import schema from './burial-form/schema';
 export default function ReviewPage(props) {
   const state = useFormikContext();
 
-  const mapData = (name, formValues, schema) => {
-    const formikValue = formValues[name];
-    const originalSchemaValue = schema[name]
-
-    if (formikValue && originalSchemaValue) {
-      if (typeof formikValue === "object") {
-        // drill into the item if it's an object
-        return {
-          [name]: mapData(item, formikValue, originalSchemaValue)
-        }
-        // for (item in formikValue) {
-        //   return mapData(item, formikValue, originalSchemaValue)
-        // }
-      }
-      else {
-        return {
-          ...originalSchemaValue,
-          value: formikValue
-        }
-      }
-    };
-  }
-
   // mockup some data to review formik context
   const pageData = {
     pages: [
       {
         title: 'Claimant information',
         id: 'claimant-information',
+        editUrl: '/claimant-information',
         fields: {
-          "fullName": mapData("fullName", state.values, schema.properties)
+          "fullName": state.values.fullName
         }
       },
       // {
