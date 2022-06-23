671bde27472a7a9738577429ab2dea75e01c9f86
BJ McCotter <bjmccotter7192@gmail.com>
2022-06-23 10:10:12 -0500

    fix: useEffect setTouched empty

--- src/routing/Page.tsx
+++ src/routing/Page.tsx
@@ -1,101 +1,8 @@
-import React, { useContext } from 'react';
-import { Form, FormikContextType, useFormikContext } from 'formik';
+import React, { useContext, useEffect } from 'react';
+import { Form, useFormikContext } from 'formik';
 import { useNavigate, To, useSearchParams } from 'react-router-dom';
-import { IFormData, PageProps } from './types';
+import { PageProps } from './types';
 import { RouterContext } from './RouterContext';
-
-const validatePage = (
-  children: JSX.Element[],
-  state: FormikContextType<unknown>
-): boolean => {
-  let isValid = true;
-
-  const values = state.values as IFormData;
-  const requiredChildren = getRequiredChildren(children);
-  const requiredChildFields = getRequiredFormikFieldNames(requiredChildren);
-
-  requiredChildFields.map((fieldName) => {
-    isValid = isFieldEmptyOrErrored(fieldName, values, state);
-  });
-
-  return isValid;
-};
-
-const isFieldEmptyOrErrored = (
-  fieldName: string,
-  values: IFormData,
-  state: FormikContextType<unknown>
-): boolean => {
-  let isValid = false;
-
-  const splitFieldNameList = fieldName.split('.');
-
-  const field = values[`${splitFieldNameList[0]}`] as IFormData;
-  console.log(field);
-  console.log(typeof field);
-
-  if (splitFieldNameList.length > 1) {
-    const field = values[`${splitFieldNameList[0]}`] as IFormData;
-    Object.entries(field).map(([key, value]) => {
-      if (key === splitFieldNameList[1]) {
-        if (
-          (typeof value === 'string' || typeof value === 'boolean') &&
-          value !== '' &&
-          state.errors !== {}
-        ) {
-          isValid = true;
-        } else {
-          isValid = false;
-        }
-      }
-    });
-  } else {
-    const fieldValue = values[`${splitFieldNameList[0]}`];
-    if (
-      (typeof fieldValue === 'string' || typeof fieldValue === 'boolean') &&
-      fieldValue !== '' &&
-      state.errors !== {}
-    ) {
-      isValid = true;
-    } else {
-      isValid = false;
-    }
-  }
-
-  return isValid;
-};
-
-const getRequiredFormikFieldNames = (children: JSX.Element[]): string[] => {
-  const requiredChildFields: string[] = [];
-
-  if (children.length > 0) {
-    children.map((childField) => {
-      requiredChildFields.push(childField.props.name);
-    });
-  }
-
-  return requiredChildFields;
-};
-
-const getRequiredChildren = (children: JSX.Element[]): JSX.Element[] => {
-  const requiredChildFields: JSX.Element[] = [];
-
-  console.log(children);
-
-  if (children.length > 0) {
-    children.forEach((child) => {
-      if (typeof child.props === 'object') {
-        const childPropsKeys = Object.keys(child.props);
-
-        if (childPropsKeys.indexOf('required') > -1) {
-          requiredChildFields.push(child);
-        }
-      }
-    });
-  }
-
-  return requiredChildFields;
-};
 
 /**
  * Renders the page contents
@@ -110,6 +17,10 @@ export default function Page(props: PageProps): JSX.Element {
 
   const { nextRoute, previousRoute } = useContext(RouterContext);
   const state = useFormikContext();
+
+  useEffect(() => {
+    state.setTouched({});
+  }, []);
 
   return (
     <div>
@@ -149,13 +60,11 @@ export default function Page(props: PageProps): JSX.Element {
         {nextRoute && (
           <button
             className="btn usa-button-primary next"
-            onClick={(event) => {
-              const isValid = validatePage(
-                props.children as JSX.Element[],
-                state
-              );
-
-              if (isValid) {
+            onClick={() => {
+              const keys = Object.keys(state.errors);
+              if (keys.length > 0) {
+                state.handleSubmit();
+              } else {
                 navigate(nextRoute as To);
               }
             }}
