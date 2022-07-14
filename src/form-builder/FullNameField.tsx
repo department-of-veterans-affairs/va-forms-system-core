faba312a23b1569f98f3afd2e95b92a5a7ce8f31
Lauren Maxwell <lauren.maxwell@adhocteam.us>
2022-07-14 15:50:12 -0700

    Adds basic idea for review page.

--- src/form-builder/FullNameField.tsx
+++ src/form-builder/FullNameField.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import { Suffixes } from '../utils/constants';
 import { FullNameProps } from './types';
 import SelectField from './SelectField';
 import TextField from './TextField';
 import { CapitalizeFirstLetter } from '../utils/helpers';
+import { PageContext } from '../routing/PageContext';
+
+import { createFieldDataFromChildren } from '../routing/Page';
+import { FieldObject, PageObject } from '../routing';
+
+const ComponentReader = (props: {
+  children: JSX.Element[];
+  fieldName: string;
+}): JSX.Element => {
+  const { children, fieldName } = props;
+  const { listOfPages, setListOfPages } = useContext(PageContext);
+
+  useEffect(() => {
+    const listOfPagesCopy = listOfPages;
+    const fieldData = createFieldDataFromChildren(children);
+
+    listOfPagesCopy.forEach((page, index) => {
+      const indexOfField = page.fields.findIndex(
+        (field) => field.name === fieldName
+      );
+      if (indexOfField >= 0) {
+        page.fields[indexOfField].children = fieldData;
+      }
+    });
+
+    if (listOfPagesCopy.length > 0) {
+      setListOfPages(listOfPagesCopy);
+    }
+  }, [listOfPages]);
+
+  return <>{children}</>;
+};
 
 const FullNameField = (props: FullNameProps): JSX.Element => {
   const fieldName = props.name;
@@ -17,7 +49,7 @@ const FullNameField = (props: FullNameProps): JSX.Element => {
   };
 
   return (
-    <>
+    <ComponentReader fieldName={fieldName}>
       <TextField
         id={`${fieldName}FirstName`}
         name={`${fieldName}.first`}
@@ -44,7 +76,7 @@ const FullNameField = (props: FullNameProps): JSX.Element => {
           <option key={`${idx}-${suffix}`}>{suffix}</option>
         ))}
       </SelectField>
-    </>
+    </ComponentReader>
   );
 };
 
---
+++ src/routing/PageContext.tsx
@@ -0,0 +1,100 @@
+import React, { ReactElement, useContext, useEffect, useState } from 'react';
+import { RouteObject } from 'react-router-dom';
+import {
+  IPageContext,
+  PageContextProps,
+  PageObject,
+  FieldObject,
+} from './types';
+
+import Page from './Page';
+import { update } from 'lodash';
+
+const getPageFields = (children: React.ReactNode): FieldObject[] => {
+  return [];
+};
+
+export function createPagesFromChildren(
+  children: React.ReactNode,
+  url = ''
+): PageObject[] {
+  let pages: PageObject[] = [];
+
+  React.Children.forEach(children, (element) => {
+    if (!React.isValidElement(element)) {
+      // Ignore non-elements. This allows people to more easily inline
+      // conditionals in their route config.
+      return;
+    }
+
+    if (
+      element.type === React.Fragment ||
+      (element.type as React.JSXElementConstructor<any>)?.name === 'Routes' ||
+      (element.type as React.JSXElementConstructor<any>)?.name === 'Route'
+    ) {
+      // Transparently support React.Fragment and its children.
+
+      pages = [
+        ...pages,
+        ...createPagesFromChildren(element.props.children, ''),
+      ];
+      // pages.push.apply(
+      //   pages,
+
+      // );
+      return;
+    }
+
+    // may not be needed here
+    if ((element.type as React.JSXElementConstructor<any>) !== Page) {
+      throw new Error(
+        `[${
+          typeof element.type === 'string' ? element.type : element.type.name
+        }] is not a <Page> component.`
+      );
+    }
+
+    const page: PageObject = {
+      title: element.props.title,
+      id: '',
+      path: '',
+      fields: getPageFields(element),
+    };
+
+    // if (element.props.children) {
+    //   page.children = createPagesFromChildren(element.props.children);
+    // }
+
+    pages.push(page);
+  });
+
+  return pages;
+}
+
+const PageContextInitialValues = {
+  listOfPages: [],
+  setListOfPages: (pages: PageObject[]) => null,
+};
+
+export const PageContext = React.createContext<IPageContext>(
+  PageContextInitialValues
+);
+
+export function PageContextProvider(props: PageContextProps): JSX.Element {
+  const [listOfPages, setListOfPages] = useState<PageObject[]>([]);
+
+  const updatePages = (pages: PageObject[]) => {
+    setListOfPages(pages);
+  };
+
+  return (
+    <PageContext.Provider
+      value={{
+        listOfPages: listOfPages,
+        setListOfPages: updatePages,
+      }}
+    >
+      {props.children}
+    </PageContext.Provider>
+  );
+}
