9df63eed9175b87944ea74dd632db995d37463ad
BJ McCotter <bj.mccotter@adhocteam.us>
2022-08-23 13:10:41 -0500

    fix(#543): fix typo in comment

--- examples/index.jsx
+++ examples/index.jsx
@@ -10,7 +10,7 @@ import {defineCustomElements} from '@department-of-veterans-affairs/component-li
 import schema from './burial-form/schema';
 import {transformJSONSchema} from "@department-of-veterans-affairs/va-forms-system-core";
 
-// USE THIS IF YOU DON'T WANT TO FILL OUR THE FORM
+// USE THIS IF YOU DON'T WANT TO FILL OUT THE FORM
 // Simply change the initialValues to use testData
 // import { testData } from './testFormikData';
 
