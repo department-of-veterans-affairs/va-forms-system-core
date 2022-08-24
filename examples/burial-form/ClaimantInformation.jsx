f2c8fad06c4d167da0ecf7e81397a5aabde64568
Anish <obliviga@users.noreply.github.com>
2022-08-24 13:38:51 -0700

    [#564] Update form labels to be more specific (#582)

--- examples/burial-form/ClaimantInformation.jsx
+++ examples/burial-form/ClaimantInformation.jsx
@@ -68,7 +68,7 @@ export default function ClaimantInformation(props) {
               <TextField
                 className="vads-u-border-color--primary-alt-light vads-u-border-left--4px vads-u-padding-left--2 vads-u-padding-y--0p5 vads-u-margin-left--neg2p5"
                 name="relationship.other"
-                label="Please specify"
+                label="Please specify claimant's relationship to deceased Veteran"
                 required={values.relationship.type === 'other'}
               />
               <CheckboxField
