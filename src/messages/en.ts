0da78572a501a4c44f080eeeb82118235c8466d9
Anish Kshatriya <obliviga@gmail.com>
2022-09-07 14:59:16 -0700

    fix(#586): Add comment in the translations file explaining that it’s a future feature to read default error messages from that file

--- src/messages/en.ts
+++ src/messages/en.ts
@@ -1,6 +1,14 @@
 import { Messages } from './types';
 
 export const messages: Messages = {
+  required: {
+    /*
+      This default value is not tied to any functionality. It is rather a placeholder for a future feature where the default error message should be rendered from the input's label. That label should be injected into this object, instead of from validation.js
+      
+      The feature should be added in relation to this issue: https://github.com/department-of-veterans-affairs/va-forms-system-core/issues/586
+    */
+    default: '{{ input_label }} is required',
+  },
   length: {
     default: 'Please provide one or more entries',
     min: 'The minimum amount of entries is x',
